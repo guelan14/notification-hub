4df4b7712f1975cc192da542af63601bf751136e
guelan <miguelangelneumann@gmail.com>
2026-05-19 22:56:03 +0000

    chore: add prettier and update eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(js.configs.recommended, tseslint.configs.recommended);
